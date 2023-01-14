2d7b6086e0b0249be2bd04f431e3c7003c792669
MERT ANNAC <mertannac91@gmail.com>
2023-01-14 14:20:30 +0300

    14.01.2023

--- src/test/java/tests/day1/IlkTestPriority.java
+++ src/test/java/tests/day1/IlkTestPriority.java
@@ -24,12 +24,3 @@ public class IlkTestPriority {
 }
 
 
-/*
-echo "# TestNgPratik" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/MertAnnacc/TestNgPratik.git
-git push -u origin main
- */
